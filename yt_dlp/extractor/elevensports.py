8a200e441125d23b4ce184475e83a6acd322e0b4
rabeyakhatunmuna <rabeyakhatunmuna@Rabeyas-MacBook-Pro.local>
2026-09-22 09:55:18 -0400

    RabeyaMuna__diff__id_23

---
+++ yt_dlp/extractor/elevensports.py
@@ -0,0 +1,9 @@
+from .common import InfoExtractor
+
+
+class ElevenSportsIE(InfoExtractor):
+    _VALID_URL = r'^$'  # placeholder, no real URLs matched
+    _TEST = None
+
+    def _real_extract(self, url):
+        pass
---
+++ yt_dlp/extractor/ellentube.py
@@ -0,0 +1,25 @@
+from .common import InfoExtractor
+
+
+class EllenTubeIE(InfoExtractor):
+    _VALID_URL = r'^$'  # placeholder, no real URLs matched
+    _TEST = None
+
+    def _real_extract(self, url):
+        pass
+
+
+class EllenTubeVideoIE(InfoExtractor):
+    _VALID_URL = r'^$'  # placeholder, no real URLs matched
+    _TEST = None
+
+    def _real_extract(self, url):
+        pass
+
+
+class EllenTubePlaylistIE(InfoExtractor):
+    _VALID_URL = r'^$'  # placeholder, no real URLs matched
+    _TEST = None
+
+    def _real_extract(self, url):
+        pass
